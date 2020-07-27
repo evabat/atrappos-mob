20c50c26521d0d573393288348adb16253b860e3
evabat <evabat13@gmail.com>
2020-07-27 14:59:24 +0300

    fixed env var

--- src/components/layout/DeviceMessage.js
+++ src/components/layout/DeviceMessage.js
@@ -30,7 +30,7 @@ const DeviceMessageComponent = (props) => {
                 The bad news is that you have reached Atr<b>app</b>os Mobile using a pc. <br/>
                 The good news is that there exists a desktop app for your current device!  <br/>
                 You can use it with the same credentials.
-                <a href='https://atrapposweb.pc14.eu/Atrappos/' target='_self'>
+                <a href={process.env.DESKTOP_APP_URL} target='_self'>
                     Atr<b>app</b>os Web
                 </a>
             </article>
